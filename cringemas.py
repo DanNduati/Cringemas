0ce47cbd82066dae2785948edd659fa71a039d30
Daniel Nduati <dannduati2@gmail.com>
2021-12-27 00:10:21 +0300

    Update main module

--- cringemas.py
+++ cringemas.py
@@ -1,17 +1,23 @@
 import config
+import asyncio
 from send_sms import AtSms
 from send_sms import TwilioSms
+from contacts import gen_dict
+from fetch_message import fetch_data
 
-#create an AT sms object
+#create an AT and twilio sms object
 at_sms = AtSms(config.username,config.api_key,config.short_code)
 twilio_sms = TwilioSms(config.account_sid,config.auth_token)
 
-recepients = ["+254723060846"]
-message = "Hello there!"
-
 def main():
-    at_sms.send(recepients,message)
-    twilio_sms.send(config.sender,recepients[0],message)
+    contact_data = gen_dict()
+    names = list(contact_data.keys())
+    for name in names:
+        msg_header = f"Dear {name},\n"
+        msg:str = asyncio.run(fetch_data())
+        message = msg_header+msg+"\nFrom DanieL"
+        at_sms.send([contact_data.get(name)],message)
+        twilio_sms.send(config.sender,contact_data.get(name),message)
 
 if __name__ == "__main__":
     main()
