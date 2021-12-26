d0480b1c5ddecb9c5d59a350ce8288a5981db4b6
Daniel Nduati <dannduati2@gmail.com>
2021-12-26 20:07:53 +0300

    Update config module
    Fetch and load twilio env vars

--- config.py
+++ config.py
@@ -11,3 +11,6 @@ api_key = os.environ.get('AT_APIKEY',None)
 short_code = os.environ.get('AT_SHORTCODE',None)
 
 #twilio
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID',None)
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN',None)
+sender = os.environ.get('TWILIO_PHONE_NUMBER',None)
